f5f1f6ecdc2627ddeefd3486ef3def94b4b0f331
Adriano Borges Silva <120127965+AdrianoBorgesSilva@users.noreply.github.com>
2024-12-31 15:46:27 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/project/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/project/workshopmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.project.workshopmongodb.domain.Post;
 import com.project.workshopmongodb.domain.User;
+import com.project.workshopmongodb.dto.AuthorDTO;
 import com.project.workshopmongodb.repository.PostRepository;
 import com.project.workshopmongodb.repository.UserRepository;
 
@@ -35,10 +36,10 @@ public class Instantiation implements CommandLineRunner{
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2028"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
-
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2028"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1));
     }
     
